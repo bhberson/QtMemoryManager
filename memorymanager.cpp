aa2576768ac60872575cf1ead61ccb75d61f7985
Brody Berson <brody.berson@gmail.com>
2014-03-30 19:07:33 -0400

    Set up Main Window Files and Header
    The CPP still needs a lot to be implemented

--- memorymanager.cpp
+++ memorymanager.cpp
@@ -12,3 +12,51 @@ MemoryManager::~MemoryManager()
 {
     delete ui;
 }
+
+
+void MemoryManager::loadInput()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Please select an acceptable file to input", QString(), "*.txt *.dat", 0, QFileDialog::ReadOnly);
+    if(fileName.isEmpty())
+        return;
+    QFile inputFile(filNeame);
+    if(!inputFile.open(QFile::ReadOnly | QFile::Text))
+        return;
+
+    QTextStream inputTextStream(&inputFile);
+    inputTextEdit->setPlainText(inputTextStream.readAll());
+}
+void MemoryManager::next()
+{
+    // Needs to Be Implemented
+}
+
+void MemoryManager::createPages(int numPages)
+{
+    // Needs to Be Implemented
+}
+
+void MemoryManager::start()
+{
+    // Needs to Be Implemented
+}
+
+void MemoryManager::insertPage(int pageIndex, Page page)
+{
+    // Needs to Be Implemented
+}
+
+void MemoryManager::removePage(int pageIndex)
+{
+    // Needs to Be Implemented
+}
+
+void MemoryManager::showError(const QString &title, const QString &message)
+{
+    // Needs to Be Implemented
+}
+
+void MemoryManager::showMessage(const QString &message)
+{
+    // Needs to Be Implemented
+}
