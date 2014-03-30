a9ab7b1f86d91d2efa1cc0a931c94ea4515396d4
Brody Berson <brody.berson@gmail.com>
2014-03-30 19:19:49 -0400

    CoreStructs Header File
    Added Methods and Variables I’ll need later

--- corestructs.h
+++ corestructs.h
@@ -7,11 +7,24 @@ class coreStructs : public QObject
 {
     Q_OBJECT
 public:
-    explicit coreStructs(QObject *parent = 0);
+    explicit coreStructs(int pageSize, int numPages, QObject *parent = 0);
+    ~coreStructs();
 
 signals:
+    void pagesCreated(int numPages);
+    void lineBeingParsed(int lineNumber);
+    void pageInserted(int pageIndex, Page page);
+    void pageRemoved(int pageIndex);
+    void showError(const QString &title, const QString &message);
+    void showMessage(const QString &message);
+
 
 public slots:
+    void next(QString command);
+
+private:
+    int pageSize;
+    int numPages;
 
 };
 
